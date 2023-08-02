6057f2695a8d0f7e4954e82a253d47c332f0368f
EmilHoejrup <emilhoejrup@gmail.com>
2023-08-02 14:34:08 +0200

    add basic menu

--- pspm.py
+++ pspm.py
@@ -5,30 +5,65 @@ from zxcvbn import zxcvbn
 
 
 def init(user):
-    master_password = create_master_password()
-    generate_encryption_key(master_password)
-    #TODO use user
+    master_password = create_master_password(user)
+    hashed_master = hash_password(master_password)
+    write_config(user, master_password)
+    menu(user)
 
 
-def create_master_password():
+def hash_password(password):
+    pass
+
+def create_master_password(user):
     while True:
         password = get_password()
-        results = zxcvbn(password)
+        results = zxcvbn(password, user_inputs=user)
         if results['score'] != 4:
             print("password not strong enough ", results['feedback']['suggestions'])
         else:
-            print("pspm vault created")
+            print("pspm vault created for user", user)
             return password
     
 
 def generate_encryption_key(master):
     pass
 
+def login(user):
+    while True:
+        if get_password() == "123":
+            menu(user)
+        else:
+            print("incorrect password or username")
 
-def write_config(hashed_master):
-    with open(".config", 'w') as config:
+
+def menu(user):
+    menu_options = '''\
+Options:
+
+l : Show list of sites
+g : Generate new password
+x : Exit
+rm: Remove site
+'''
+    while True:
+        print(menu_options)
+        arg = input("> ").lower()
+        if arg == "l":
+            show_list(user)
+        elif arg == 'g':
+            generate_password(user)
+        elif arg == 'x':
+            return
+
+  
+def show_list(user):
+    pass
+
+def write_config(user, hashed_master):
+    config_file = ".config_" + user
+    with open(config_file, 'w') as config:
         config.write(hashed_master)
-    os.chmod(".config", 0o600)
+    os.chmod(config_file, 0o600)
 
 def get_password():
     return getpass.getpass("Enter master password: ")
@@ -46,19 +81,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         "pspm (pretty secure password manager)")
     parser.add_argument("--init", "-i", metavar="user",
-                        help="initialize pspm vault")
-    parser.add_argument("-s", "--show", metavar="service",
-                        help="show password for a service")
-    parser.add_argument("-g", "--generate",
-                        metavar="service", help="generate password for a service")
+                        help="initialize new pspm vault for <user>")
+    parser.add_argument("--login", "-l", metavar="user",
+                        help="login as <user>")
 
     args = parser.parse_args()
 
     if args.init:
         init(args.init)
-    elif args.show:
-        show_password(args.show)
-    elif args.generate:
-        generate_password(args.generate)
+    elif args.login:
+        login(args.login)
     else:
         parser.print_help()

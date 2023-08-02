260ce2ec7092fd224f93ce12ef4b9788bc9196a6
EmilHoejrup <emilhoejrup@gmail.com>
2023-08-02 12:08:47 +0200

    add create_master_password() with zxcvbn strenght checking

--- pspm.py
+++ pspm.py
@@ -1,6 +1,34 @@
 import argparse
 import getpass
+import os
+from zxcvbn import zxcvbn
 
+
+def init(user):
+    master_password = create_master_password()
+    generate_encryption_key(master_password)
+    #TODO use user
+
+
+def create_master_password():
+    while True:
+        password = get_password()
+        results = zxcvbn(password)
+        if results['score'] != 4:
+            print("password not strong enough ", results['feedback']['suggestions'])
+        else:
+            print("pspm vault created")
+            return password
+    
+
+def generate_encryption_key(master):
+    pass
+
+
+def write_config(hashed_master):
+    with open(".config", 'w') as config:
+        config.write(hashed_master)
+    os.chmod(".config", 0o600)
 
 def get_password():
     return getpass.getpass("Enter master password: ")
@@ -9,13 +37,16 @@ def get_password():
 def show_password(service):
     if get_password() == "123":
         print("works")
+        
+def generate_password(service):
+    pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        "pspm (pretty secure password manager) is a simple cli-based password manager")
-    parser.add_argument("--init", metavar="user",
-                        help="initialize pspm vault for user")
+        "pspm (pretty secure password manager)")
+    parser.add_argument("--init", "-i", metavar="user",
+                        help="initialize pspm vault")
     parser.add_argument("-s", "--show", metavar="service",
                         help="show password for a service")
     parser.add_argument("-g", "--generate",
@@ -23,5 +54,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.show:
+    if args.init:
+        init(args.init)
+    elif args.show:
         show_password(args.show)
+    elif args.generate:
+        generate_password(args.generate)
+    else:
+        parser.print_help()

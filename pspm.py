8e5d37385d9c2a312962a27567c2e6c8dcf9054d
EmilHoejrup <emilhoejrup@gmail.com>
2023-08-03 18:47:45 +0200

    format with black

--- pspm.py
+++ pspm.py
@@ -26,7 +26,7 @@ def hash_password(password):
     # not sure if these parameters are optimal
     params = {"time_cost": 30, "memory_cost": 102400, "parallelism": 8, "hash_len": 256}
     return argon2.PasswordHasher(**params).hash(password.encode())
-    
+
 
 def create_master_password(user):
     while True:
@@ -34,21 +34,21 @@ def create_master_password(user):
         password = get_password()
         results = zxcvbn(password, user_inputs=user)
         # ensures that password has highest possible zxcvbn score
-        if results['score'] != 4:
-            print("password not strong enough ", results['feedback']['suggestions'])
+        if results["score"] != 4:
+            print("password not strong enough ", results["feedback"]["suggestions"])
         else:
             try:
                 cwd = os.getcwd()
                 path = cwd + "/" + user + "_vault"
                 os.mkdir(path)
-                #TODO proper chmod
+                # TODO proper chmod
                 # os.chmod(path, 0o600)
                 print("pspm vault created for user", user)
                 return password
             except FileExistsError:
-                print("user already exists! try a new username")
+                print("user already exists! use another username")
                 sys.exit()
-                
+
 
 def login(user):
     while True:
@@ -71,10 +71,11 @@ def authenticate(stored, provided):
 
 def get_master(user):
     with open(".config_" + user) as config:
-       return config.read() 
+        return config.read()
+
 
 def menu(user):
-    menu_options = '''\
+    menu_options = """\
 
 Options:
 
@@ -82,41 +83,43 @@ l  : Show list of sites
 g  : Generate new password
 x  : Exit
 rm : Remove site
-'''
+"""
     while True:
         print(menu_options)
         arg = input("> ").lower()
         if arg == "l":
             show_password(user)
-        elif arg == 'g':
+        elif arg == "g":
             generate_password(user)
-        elif arg == 'x':
+        elif arg == "x":
             sys.exit()
 
 
 # writes the master password to the users .config file and ensures only user has read and write permissions to it
 def write_config(user, hashed_master):
     config_file = ".config_" + user
-    with open(config_file, 'w') as config:
+    with open(config_file, "w") as config:
         config.write(hashed_master)
     os.chmod(config_file, 0o600)
+
 
 def get_password():
     return getpass.getpass("Enter master password: ")
 
-        
+
 def generate_password(user):
-    service = input('''enter the name of the service you want to generate a password for
-> ''')
+    service = input(
+        """enter the name of the service you want to generate a password for
+> """
+    )
     charset = string.ascii_letters + string.punctuation + string.digits
     salt = os.urandom(16)
     encryption_key = generate_encryption_key(salt, user)
     s = secrets.SystemRandom()
     s.seed(encryption_key)
-    password = ''.join(s.choice(charset) for _ in range(16))
+    password = "".join(s.choice(charset) for _ in range(16))
     write_site(user, service, password)
     copy_to_clipboard(password)
-
 
 
 def write_site(user, service, password):
@@ -124,51 +127,53 @@ def write_site(user, service, password):
     path = cwd + "/" + user + "_vault/" + service
     salt = os.urandom(16)
     # to avoid bug with newline in file - temporary fix
-    while b'\n' in salt:
+    while b"\n" in salt:
         salt = os.urandom(16)
-    with open(path, 'wb') as file:
+    with open(path, "wb") as file:
         cipher = Fernet(base64.urlsafe_b64encode(generate_encryption_key(salt, user)))
         encrypted_password = cipher.encrypt(password.encode())
-        file.writelines([salt + b'\n', encrypted_password])
+        file.writelines([salt + b"\n", encrypted_password])
     os.chmod(path, 0o600)
-    
+
+
 def show_password(user):
     service = input("what site do you want the password for? \n > ")
     cwd = os.getcwd()
     path = cwd + "/" + user + "_vault/" + service
     try:
-        with open(path, 'rb') as file:
+        with open(path, "rb") as file:
             salt, encrypted_password = file.read().splitlines()
         cipher = Fernet(base64.urlsafe_b64encode(generate_encryption_key(salt, user)))
         decrypted_password = cipher.decrypt(encrypted_password).decode()
         copy_to_clipboard(decrypted_password)
     except IOError:
         print("site " + service + " not found")
- 
+
+
 def generate_encryption_key(salt, user):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256,
         length=32,
         salt=salt,
         iterations=100000,
-        backend=default_backend
+        backend=default_backend,
     )
     return kdf.derive(get_master(user).encode())
+
 
 def copy_to_clipboard(password):
     pyperclip.copy(password)
     print("Password copied to clipboard. It will be deleted in 5 sec")
     time.sleep(5)
-    pyperclip.copy('')
+    pyperclip.copy("")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        "pspm (pretty secure password manager)")
-    parser.add_argument("--init", "-i", metavar="user",
-                        help="initialize new pspm vault for <user>")
-    parser.add_argument("--login", "-l", metavar="user",
-                        help="login as <user>")
+    parser = argparse.ArgumentParser("pspm (pretty secure password manager)")
+    parser.add_argument(
+        "--init", "-i", metavar="user", help="initialize new pspm vault for <user>"
+    )
+    parser.add_argument("--login", "-l", metavar="user", help="login as <user>")
 
     args = parser.parse_args()
 
